c1bfdabcfb344463dd6783cd0f7b7500e57f4172
witmicko <mogrodni@redhat.com>
2018-04-25 15:55:17 +0100

    bits n bobs

--- main.go
+++ main.go
@@ -7,7 +7,6 @@ import (
 	"log"
 	"os"
 	"strings"
-
 	"github.com/adlio/trello"
 	"github.com/andygrunwald/go-jira"
 )
@@ -30,7 +29,7 @@ func createJiraClient() {
 
 	fmt.Println(tp)
 
-	c, err := jira.NewClient(tp.Client(), tp.AuthURL)
+	c, err := jira.NewClient(tp.Client(), *flagJiraHost)
 	if err != nil {
 		panic(err)
 	}
@@ -51,39 +50,70 @@ func AddToJira() {
 }
 
 func main() {
-
 	flag.Parse()
 	createJiraClient()
+
+	//get sample epic
+	sampleEpic, _, _ := jiraClient.Issue.Get("AEROGEAR-2363", nil)
+
+	stories := getStoriesFromTrello()
+
+	for _, story := range stories{
+		epic := buildEpic(story, sampleEpic)
+		fmt.Println(epic)
+		//jiraClient.Issue.Create(&epic)
+	}
+
+}
+
+func getStoriesFromTrello()[]Story{
 	appKey := os.Getenv("TRELLO_APP_KEY")
 	token := os.Getenv("TRELLO_TOKEN")
+
 	client := trello.NewClient(appKey, token)
 	board, err := client.GetBoard("VrnvGm7P", trello.Defaults())
 	if err != nil {
 		log.Fatal(err)
-		return
+		return nil
 	}
 	lists, err := board.GetLists(trello.Defaults())
 	if err != nil {
 		log.Fatal(err)
-		return
+		return nil
 	}
+	stories := make([]Story, 0)
 	for _, l := range lists {
 		if strings.TrimSpace(strings.ToLower(l.Name)) == "backlog" {
 			fmt.Println("found backlog", l)
 			args := trello.Defaults()
 			args["checklists"] = "all"
-			cards, err := l.GetCards(args)
-			if err != nil {
-				log.Fatal(err)
-				return
-			}
-			for _, c := range cards {
-
-				checkLists := c.Checklists
-				fmt.Println("checklists", checkLists)
-
-			}
+			args["attachments"] = "true"
+			cards, _ := l.GetCards(args)
+			stories = processCards(cards)
 		}
 	}
+	return stories
+}
 
+func processCards(cards []*trello.Card) []Story{
+	stories := make([]Story, 0, len(cards))
+	for _, c := range cards {
+		name := c.Name
+		storyPoints := extractStoryPoints(name)
+		labels := extractLabels(c.Labels)
+		checklists := extractChecklists(c.Checklists)
+		cardUrl := c.ShortUrl
+		attachments := extractAttachments(c.Attachments)
+
+		stories = append(stories, Story{
+			name,
+			storyPoints,
+			labels,
+			checklists,
+			cardUrl,
+			attachments,
+			c.Desc,
+		})
+	}
+	return stories
 }
